d6516a818ab3681f5448fc58c4657f9a8de5849b
Agnieszka Typiak <agnieszka.typiak@mailbox.org>
2023-03-23 08:42:06 +0100

    Rozwiazanie_formatowanie

--- src/test/java/pl/javastart/euler2/Euler2Test.java
+++ src/test/java/pl/javastart/euler2/Euler2Test.java
@@ -1,71 +1,65 @@
 package pl.javastart.euler2;
 
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.junit.jupiter.api.Assertions.assertAll;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static pl.javastart.euler2.Euler2.sumUpEvenElementsWithLimit;
 
 import java.io.ByteArrayOutputStream;
-import java.util.Optional;
 
-//import org.junit.jupiter.api.Assertions;
 import org.assertj.core.api.SoftAssertions;
 import org.junit.jupiter.api.Test;
 import org.assertj.core.api.Assertions;
 
-
 public class Euler2Test {
-	private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
 
-	@Test
-	public void shouldReturnError() {
-		//given
-		int elementsNo = 10;
-		int biggestElement = 1000;
+    @Test
+    public void shouldReturnError() {
+        //given
+        int elementsNo = 10;
+        int biggestElement = 1000;
 
-		//when
-		FibonacciSequence fibonacci = new FibonacciSequence(elementsNo);
-		int sumOfEvenElements = sumUpEvenElementsWithLimit(fibonacci, biggestElement);
+        //when
+        FibonacciSequence fibonacci = new FibonacciSequence(elementsNo);
+        int sumOfEvenElements = sumUpEvenElementsWithLimit(fibonacci, biggestElement);
 
-		//then
-		SoftAssertions softAssertions = new SoftAssertions();
-		softAssertions.assertThat(sumOfEvenElements).isEqualTo(0);
-		softAssertions.assertThat(outContent.toString()).contains("Zbyt krótka lista.");
+        //then
+        SoftAssertions softAssertions = new SoftAssertions();
+        softAssertions.assertThat(sumOfEvenElements).isEqualTo(0);
+        softAssertions.assertThat(outContent.toString()).contains("Zbyt krótka lista.");
 
-	}
+    }
 
-	@Test
-	public void shouldReturn44() {
-		//given
-		int elementsNo = 10;
-		int biggestElement = 34;
+    @Test
+    public void shouldReturn44() {
+        //given
+        int elementsNo = 10;
+        int biggestElement = 34;
 
-		//when
-		FibonacciSequence fibonacci = new FibonacciSequence(elementsNo);
-		int sumOfEvenElements = sumUpEvenElementsWithLimit(fibonacci, biggestElement);
+        //when
+        FibonacciSequence fibonacci = new FibonacciSequence(elementsNo);
+        int sumOfEvenElements = sumUpEvenElementsWithLimit(fibonacci, biggestElement);
 
-		//then
-		Assertions.assertThat(sumOfEvenElements).isEqualTo(44);
+        //then
+        Assertions.assertThat(sumOfEvenElements).isEqualTo(44);
 
-	}
+    }
 
-	@Test
-	public void shouldReturnCorrectFibonacciNo() {
-		//given
-		int elementsNo = 40;
-		int offset = 1;
-		double goldenRatio = 1.6180339887;
+    @Test
+    public void shouldReturnCorrectFibonacciNo() {
+        //given
+        int elementsNo = 40;
+        int offset = 1;
+        double goldenRatio = 1.6180339887;
 
-		//when
-		FibonacciSequence fibonacci = new FibonacciSequence(elementsNo);
-		int fibonacciLastIndex = fibonacci.getElementsNo()-1;
-		int fibonacciLastElement = fibonacci.getFibonacci().get(fibonacciLastIndex);
+        //when
+        FibonacciSequence fibonacci = new FibonacciSequence(elementsNo);
+        int fibonacciLastIndex = fibonacci.getElementsNo()-1;
+        int fibonacciLastElement = fibonacci.getFibonacci().get(fibonacciLastIndex);
 
-		int lastElement = (int) Math.round((Math.pow(goldenRatio, (elementsNo + offset)) - Math.pow(1 - goldenRatio, (elementsNo + offset)))
-								/ Math.sqrt(5));
+        int lastElement = (int) Math.round((Math.pow(goldenRatio, (elementsNo + offset)) - Math.pow(1 - goldenRatio, (elementsNo + offset)))
+                / Math.sqrt(5));
 
-		//then
-		Assertions.assertThat(fibonacciLastElement).isEqualTo(lastElement);
+        //then
+        Assertions.assertThat(fibonacciLastElement).isEqualTo(lastElement);
 
-	}
+    }
 }
